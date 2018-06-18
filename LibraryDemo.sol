a40ea2f78b37dcaf97bd2f48ce9ab45f488e54ca
Praveen Kumar Pandey <35739960+pp1989@users.noreply.github.com>
2018-06-19 03:30:56 +0530

    Update LibraryDemo.sol

--- LibraryDemo.sol
+++ LibraryDemo.sol
@@ -1,15 +1,5 @@
-pragma solidity ^0.4.18;
-
 library arithmatic {
-    function add(uint _a, uint _b) internal pure returns (uint) {
-        return _a + _b;
-    }
-}
-
-contract C {
-    uint sum;
-
-    function f() public {
-        sum = arithmatic.add(2, 3);
+    function add(uint _a, uint _b) pure public returns (uint) {
+        return (_a + _b);
     }
 }
